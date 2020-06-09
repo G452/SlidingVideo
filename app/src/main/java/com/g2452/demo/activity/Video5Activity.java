d757f84c636d210d543302e0b422e090932b0732
G <2587560197@qq.com>
2020-06-09 15:09:28 +0800

    同时播放+手势拖动图片

--- app/src/main/java/com/g2452/demo/activity/Video5Activity.java
+++ app/src/main/java/com/g2452/demo/activity/Video5Activity.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 import com.g2452.demo.R;
 import com.g2452.demo.view.EmptyControlVideo;
+import com.g2452.demo.view.MultiSampleVideo;
 
 /**
  * 作者：G
@@ -20,12 +21,19 @@ public class Video5Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video5);
-        EmptyControlVideo mEmptyControlVideo1 = (EmptyControlVideo) findViewById(R.id.video1);
-        EmptyControlVideo mEmptyControlVideo2 = (EmptyControlVideo) findViewById(R.id.video2);
+        MultiSampleVideo mEmptyControlVideo1 = (MultiSampleVideo) findViewById(R.id.video1);
+        MultiSampleVideo mEmptyControlVideo2 = (MultiSampleVideo) findViewById(R.id.video2);
+        //多个播放时必须在setUpLazy、setUp和getGSYVideoManager()等前面设置
+        mEmptyControlVideo1.setPlayTag("Video5Activity");
+        mEmptyControlVideo1.setPlayPosition(0);
+        mEmptyControlVideo1.setPlayTag("Video5Activity");
+        mEmptyControlVideo1.setPlayPosition(1);
         mEmptyControlVideo1.setUp("http://vicdn.baichuan99.cn/user/202004/12/18/59/1586689143613AD7CC47.mp4", true, "");
         mEmptyControlVideo2.setUp("http://vicdn.baichuan99.cn/user/202004/12/18/59/1586689143319AEAA87F.mp4", true, "");
         mEmptyControlVideo1.setVideoImg("http://vicdn.baichuan99.cn/user/202004/12/18/59/1586689143613AD7CC47.mp4");
         mEmptyControlVideo2.setVideoImg("http://vicdn.baichuan99.cn/user/202004/12/18/59/1586689143319AEAA87F.mp4");
+        mEmptyControlVideo1.setLooping(true);
+        mEmptyControlVideo2.setLooping(true);
         mEmptyControlVideo1.startPlayLogic();
         mEmptyControlVideo2.startPlayLogic();
     }
